cc5b063a9b145ddbf1d3e5f780d6a5be097b380c
Frank Sauerburger <frank@sauerburger.com>
2017-11-09 18:11:25 +0100

    Implement kway merger frontend
    Add the function kway_merge, which iteratively merges an arbitrary number of
    arrays.

--- sortednp/__init__.py
+++ sortednp/__init__.py
@@ -1,2 +1,61 @@
 
 from _sortednp import merge, intersect
+
+def resolve(o):
+    """
+    Helper function.
+
+    Check whether the given object is callable. If yes, return its return
+    value, otherwise return the object itself.
+    """
+    return o() if callable(o) else o
+
+def kway_merge(*arrays, assume_sorted=True):
+    """
+    Merge all given arrays and return the result. Depending on the optional
+    flag assume_sorted, the function sorts the arrays before merging.
+
+    The method raises a TypeError, if the array count is zero.
+
+    The arguments can load the arrays on the fly. If an argument is callable,
+    its return value is used as an array instead of the argument itself. This
+    make it possible to load one array after another to avoid having all
+    arrays in memory at the same time..
+
+    Note on the performance: The function merges the arrays one-by-one. This
+    not the most performant implementation. Use the module heapq for more
+    efficient ways to merge sorted arrays.
+    """
+    if len(arrays) == 0:
+        raise TypeError("Merge expects at least one array.")
+
+    arrays = list(arrays)
+    m = arrays.pop()
+    m = resolve(m)
+    if not assume_sorted:
+        m.sort()
+    for a in arrays:
+        a = resolve(a)
+        if not assume_sorted:
+            a.sort()
+        m = merge(m, a)
+    return m
+
+def kway_intersect(*arrays, assume_sorted=True):
+    """
+    Intersect all given arrays and return the result. Depending on the
+    optional flag assume_sorted, the function sort sorts the arrays prior to
+    intersecting.
+
+    The method raises a TypeError, if the array count is zero.
+
+    The arguments can load the arrays on the fly. If an argument is callable,
+    its return value is used as an array instead of the argument itself. This
+    make it possible to load one array after another to avoid having all
+    arrays in memory at the same time..
+
+    Note on the performance: The function intersects the arrays one-by-one.
+    This is not the most performant implementation. 
+    """
+    # start with smallest non-callable
+    pass
