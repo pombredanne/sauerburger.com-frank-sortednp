acbc58088f2b336e8f7f4fbb927371216988ed9b
Frank Sauerburger <frank@sauerburger.com>
2018-01-03 22:30:59 +0100

    Update benchmark path in pylint

--- ci/pylint.sh
+++ ci/pylint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pylint --disable=e0611,e1101,r0903 setup.py benchmark.py sortednp/*.py
+pylint --disable=e0611,e1101,r0903 setup.py ci/benchmark.py sortednp/*.py
