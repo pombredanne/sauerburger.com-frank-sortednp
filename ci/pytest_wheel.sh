10555c2f10f9c79a88453acff8862e60042ea006
Frank Sauerburger <frank@sauerburger.com>
2018-01-08 22:46:04 +0100

    Add pwd and ls in pytest_wheel

--- ci/pytest_wheel.sh
+++ ci/pytest_wheel.sh
@@ -7,6 +7,10 @@ if [ -z "${PY_VERSION}" ]; then
 fi
 
 /opt/python/${PY_VERSION}/bin/pip install wheel wheelhouse/sortednp-*-${PY_VERSION}-*.whl
+pwd
+ls -la
+cd /
+
 
 /opt/python/${PY_VERSION}/bin/pip install nose
 /opt/python/${PY_VERSION}/bin/nosetests sortednp.tests
