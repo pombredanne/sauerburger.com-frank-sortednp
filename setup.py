85e66ecc30f70f0c65148f8dd28a4d6a44c35588
Frank Sauerburger <frank@sauerburger.com>
2018-07-10 20:54:15 +0200

    Change version to 0.2.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ BACKEND = NumpyApiExtension('sortednp._internal', language='c++',
                             depends=['numpy'])
 
 setup(name='sortednp',
-      version='0.2.0-rc',
+      version='0.2.0',
       packages=["sortednp", "sortednp.tests"],
       package_dir={"": "src"},
       install_requires=['numpy>=1.14'],
