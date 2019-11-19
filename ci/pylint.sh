637c6792ff03088fa6d4ef503f4e7a52ab05f00b
Frank Sauerburger <frank@sauerburger.com>
2019-11-19 22:58:51 +0100

    Ignore import-outside-toplevel

--- ci/pylint.sh
+++ ci/pylint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-pylint --disable=e0611,e1101,r0903 ${DIR}/../setup.py ${DIR}/benchmark.py sortednp.__init__
+pylint --disable=c0415,e0611,e1101,r0903 ${DIR}/../setup.py ${DIR}/benchmark.py sortednp.__init__
